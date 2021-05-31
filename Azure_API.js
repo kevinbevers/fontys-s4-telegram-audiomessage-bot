b4057aad461377e706a4b1c652bb3b5f8b66c2a4
Hoeks,Glenn G.J.P <g.hoeks@student.fontys.nl>
2021-05-31 10:41:31 +0200

    package.json added

---
+++ Azure_API.js
@@ -0,0 +1,4 @@
+function synthesizeSpeech() {
+    const speechConfig = sdk.SpeechConfig.fromSubscription("YourSubscriptionKey", "YourServiceRegion");
+    const audioConfig = AudioConfig.fromAudioFileOutput("path/to/file.wav");
+}
