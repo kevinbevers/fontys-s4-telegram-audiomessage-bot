5d82acea52be0cff78a160ce0a31041225c4a4ee
Kevin Bevers <kevin.bevers@hotmail.com>
2021-05-31 14:08:40 +0200

    added try catch so that the microsoft env variables not being set are being ignored.

--- Azure_API.js
+++ Azure_API.js
@@ -2,8 +2,9 @@ const sdk = require("microsoft-cognitiveservices-speech-sdk");
 require('dotenv').config()
 
 function synthesizeSpeech() {
-    const speechConfig = sdk.SpeechConfig.fromSubscription(process.env.SubscriptionKey, process.env.ServiceRegion);
-    const audioConfig = sdk.AudioConfig.fromAudioFileOutput(process.env.PathApiVoice);
+    try {
+    const speechConfig = sdk.SpeechConfig.fromSubscription(process.env?.SubscriptionKey, process.env?.ServiceRegion);
+    const audioConfig = sdk.AudioConfig.fromAudioFileOutput(process.env?.PathApiVoice);
 
     const synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
     synthesizer.speakTextAsync(
@@ -12,13 +13,15 @@ function synthesizeSpeech() {
             synthesizer.close();
             if (result) {
                 // return result as stream
-                return fs.createReadStream(process.env.PathApiVoice);
+                return fs.createReadStream(process.env?.PathApiVoice);
             }
         },
         error => {
             console.log(error);
             synthesizer.close();
         });
+    }
+    catch {};
 }
 module.exports = synthesizeSpeech();
 
