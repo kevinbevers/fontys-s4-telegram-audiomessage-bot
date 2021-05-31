18a3c7dca91b43d96f082446131e6ab69d051261
Kevin Bevers <kevin.bevers@hotmail.com>
2021-05-31 13:34:56 +0200

    moved audiogeneration to seperate class

--- main.js
+++ main.js
@@ -14,10 +14,7 @@ const axios = require('axios');
 //TelegramBot
 const TeleBot = require('telebot');
 const bot = new TeleBot(process.env?.TelegramBotToken);
-//ffmpeg for image, audio and movie manipulation.
-const FfmpegCommand = require('fluent-ffmpeg');
-//file system
-const fs = require('fs');
+const audiogeneration = require('./utils/audiogeneration');
 
 app.get('/get/:variable', async function (req, res) {
 
@@ -36,7 +33,7 @@ app.get('/get/:variable', async function (req, res) {
 
   app.get('/mixaudio', async function (req, res) {
 
-    await generateCoolBoardRadioEffect('./audio/stock/HelloAndWelcome.flac');
+    await audiogeneration.generateCoolBoardRadioEffect('./audio/stock/HelloAndWelcome.flac');
 
     res.send(`Audio mixed with ffmpeg`);
   });
@@ -57,53 +54,3 @@ app.get('/get/:variable', async function (req, res) {
     console.log(`Listening on port: ${port}`); 
   });
 
-  const generateCoolBoardRadioEffect = async(inputFilePath) => {
-    const command = new FfmpegCommand();
-    const command2 = new FfmpegCommand();
-    //create audio file with voice and engine sound under.
-    await command
-    .addInput(inputFilePath)
-    .addInput('./audio/utility/RB16BengineOnboard.mp3').seekInput(40)
-    //'amix=inputs=2:duration=first:dropout_transition=0'
-    .complexFilter([{
-      filter: 'volume',
-      options: ['1.0'],
-      inputs: "0:a",
-      outputs: "[s1]"
-    },
-    {
-      filter: 'volume',
-      options: ['0.3'],
-      inputs: "1:a",
-      outputs: "[s2]"
-    },
-    {
-      filter: 'amix',
-      inputs: ["[s1]","[s2]"],
-      options: ['duration=first','dropout_transition=0']
-    }]).output('./audio/temp/overlayed.mp3').on('error', function(err) {
-      console.log(err);
-    })
-    .on('end', function() {
-      console.log('Amixed audio files together.');
-
-      //put noti sound in front of previous generated file.
-      command2.addInput('./audio/utility/f1radionotification.mp3').addInput('./audio/temp/overlayed.mp3').on('error', function(err) {
-        console.log('An error occurred: ' + err.message);
-      })
-      .on('end', function() {
-        console.log('Merged audio files successfully!');
-
-        fs.unlink('./audio/temp/overlayed.mp3', (err) => {
-          if (err) {
-            console.error(err)
-            return
-          } 
-          //file removed
-        })
-      })
-      .mergeToFile('./audio/result.mp3', './audio/temp');
-
-    })
-    .run();
-  };
